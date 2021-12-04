b32ac12c9898ee694406ef43a62748871b0f94ea
Virendra <virendra.thakur31@gmail.com>
2021-12-04 21:05:39 +0530

    BaseViewModel updated.

--- SampleAppXamarin.First/SampleAppXamarin.First/ViewModels/BaseViewModel.cs
+++ SampleAppXamarin.First/SampleAppXamarin.First/ViewModels/BaseViewModel.cs
@@ -1,10 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace SampleAppXamarin.First.ViewModels
 {
-    class BaseViewModel
+    public class BaseViewModel: IDisposable
     {
+
+
+        #region INotifyPropertyChanged implementation
+
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler IsActiveChanged;
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+
+
+        protected new void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+        #endregion
+        #region IDisposable implementation
+
+        public virtual void Dispose()
+        {
+        }
+
+        #endregion
+
     }
 }
