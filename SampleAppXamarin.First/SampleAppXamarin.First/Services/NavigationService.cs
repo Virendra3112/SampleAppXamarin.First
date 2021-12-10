8800af6f742d9ede23cb3913df838488245cab37
Virendra <virendra.thakur31@gmail.com>
2021-12-10 20:01:28 +0530

    NavigationService updated.

--- SampleAppXamarin.First/SampleAppXamarin.First/Services/NavigationService.cs
+++ SampleAppXamarin.First/SampleAppXamarin.First/Services/NavigationService.cs
@@ -4,7 +4,53 @@ using System.Text;
 
 namespace SampleAppXamarin.First.Services
 {
-    class NavigationService
+    public class NavigationService : INavigationService
     {
+        private readonly Dictionary<string, Type> _pageKeyDictionary;
+        public NavigationService()
+        {
+            _pageKeyDictionary = new Dictionary<string, Type>();
+        }
+        public void Configure(string pageKey, Type pageType)
+        {
+            lock (_pageKeyDictionary)
+            {
+                if (_pageKeyDictionary.ContainsKey(pageKey))
+                {
+                    _pageKeyDictionary[pageKey] = pageType;
+                }
+                else
+                {
+                    _pageKeyDictionary.Add(pageKey, pageType);
+                }
+            }
+        }
+
+        public string CurrentPageKey { get; set; }
+
+        public void GoBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HideDrawerMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NavigateTo(string pageKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NavigateTo(string pageKey, object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopToRoot()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
