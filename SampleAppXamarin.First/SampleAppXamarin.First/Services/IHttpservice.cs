8f4a74628f9532ec3b4914c580d0f7de66871c9b
Virendra <virendra.thakur31@gmail.com>
2021-12-11 23:35:21 +0530

    IHttpservice updated.

--- SampleAppXamarin.First/SampleAppXamarin.First/Services/IHttpservice.cs
+++ SampleAppXamarin.First/SampleAppXamarin.First/Services/IHttpservice.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SampleAppXamarin.First.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace SampleAppXamarin.First.Services
 {
     public interface IHttpservice
     {
+        Task<ResponseModel<string>> SendRequestAsync(HttpMethod method, string url, string content = null, int timeout = 60);
     }
 }
