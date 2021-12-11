17994ce56e74092dd7eec00603621a6c142d56ca
Virendra <virendra.thakur31@gmail.com>
2021-12-11 23:32:53 +0530

    ResponseModel added.

---
+++ SampleAppXamarin.First/SampleAppXamarin.First/Models/ResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace SampleAppXamarin.First.Models
+{
+    public class ResponseModel<T>
+    {
+        public HttpStatusCode Status { get; set; }
+
+        public bool IsSuccess
+        {
+            get
+            {
+                return Status == HttpStatusCode.OK || Status == HttpStatusCode.Created;
+            }
+
+        }
+        public T Result { get; set; }
+    }
+}
+
