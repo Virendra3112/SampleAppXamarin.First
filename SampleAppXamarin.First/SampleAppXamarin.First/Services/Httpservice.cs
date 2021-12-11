ddd590205a3d6d385ba0c6afb6a62dc887e8943c
Virendra <virendra.thakur31@gmail.com>
2021-12-11 23:36:11 +0530

    Httpservice added.

---
+++ SampleAppXamarin.First/SampleAppXamarin.First/Services/Httpservice.cs
@@ -0,0 +1,17 @@
+﻿using SampleAppXamarin.First.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAppXamarin.First.Services
+{
+    public class Httpservice : IHttpservice
+    {
+        public Task<ResponseModel<string>> SendRequestAsync(HttpMethod method, string url, string content = null, int timeout = 60)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
