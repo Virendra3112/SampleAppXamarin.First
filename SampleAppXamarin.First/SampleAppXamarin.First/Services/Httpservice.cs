56c0a507a746d51832b182222529e1657df6c86d
Virendra <virendra.thakur31@gmail.com>
2021-12-12 23:23:19 +0530

    commit.

--- SampleAppXamarin.First/SampleAppXamarin.First/Services/Httpservice.cs
+++ SampleAppXamarin.First/SampleAppXamarin.First/Services/Httpservice.cs
@@ -1,8 +1,6 @@
 ﻿using SampleAppXamarin.First.Models;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SampleAppXamarin.First.Services
